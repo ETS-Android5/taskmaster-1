43d5bdc228bceaab3b65468d326af6014fbecb82
Eddward-Regalado <tregalado30@gmail.com>
2022-03-22 16:43:33 -0700

    finished UI elements, added screenshots, added apk build file, updated README

--- app/src/main/java/com/regalado/taskmaster/AddTaskActivity.java
+++ app/src/main/java/com/regalado/taskmaster/AddTaskActivity.java
@@ -2,13 +2,36 @@ package com.regalado.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AddTaskActivity extends AppCompatActivity {
 
+    public String TAG = "AddTaskActivity";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        // target our button
+        Button submitButton = (Button) findViewById(R.id.addTaskButton);
+
+        // create onClickListener
+        submitButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                System.out.println("submitted!");
+                Log.e(TAG, "Logging");
+                ((TextView)findViewById(R.id.submitTextView)).setText(R.string.submitted);
+            }
+        });
     }
 }
